8e8ef2af317b3ed8e1a6f7d0f750821a22bdb7ff
FedEgues <eguesf@gmail.com>
2021-12-07 19:43:47 -0300

    YML
    Se actualiza YML, se configura para una base de datos llamada:RestoApp

--- src/test/java/RestoApp/ApplicationTests.java
+++ src/test/java/RestoApp/ApplicationTests.java
@@ -1,13 +1,13 @@
-package RestoApp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package RestoApp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
