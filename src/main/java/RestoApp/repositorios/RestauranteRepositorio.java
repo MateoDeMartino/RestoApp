071d894713dcd4ab692bd03bc77c00b0b36bf30f
MateoDeMartino <mateodemartino@gmail.com>
2022-01-05 20:46:32 -0300

    restaurante controlador
    metodos: guardar restaurante
    modificar restaurante
    buscar restaurante
    mostrar restaurantes
    eliminar restaurante

--- src/main/java/RestoApp/repositorios/RestauranteRepositorio.java
+++ src/main/java/RestoApp/repositorios/RestauranteRepositorio.java
@@ -1,20 +1,20 @@
-//
-//package RestoApp.repositorios;
-//
-//import RestoApp.Entidades.Restaurante;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface RestauranteRepositorio extends JpaRepository<Restaurante, String> {
-//
-//    @Query("SELECT c FROM Restaurante c WHERE c.nombre = :nombre")
-//    public Restaurante buscarRestaurantePorNombre(@Param("nombre") String nombre);
-//    
-//    @Query("SELECT c FROM Restaurante c WHERE c.zona = :zona")
-//    public Restaurante buscarRestaurantePorZona(@Param("zona") String nombre);
-//
-//}
+
+package RestoApp.repositorios;
+
+import RestoApp.Entidades.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestauranteRepositorio extends JpaRepository<Restaurante, String> {
+
+    @Query("SELECT c FROM Restaurante c WHERE c.nombre = :nombre")
+    public Restaurante buscarRestaurantePorNombre(@Param("nombre") String nombre);
+    
+    @Query("SELECT c FROM Restaurante c WHERE c.zona = :zona")
+    public Restaurante buscarRestaurantePorZona(@Param("zona") String nombre);
+
+}
 
--- src/main/java/RestoApp/servicios/RestauranteServicio.java
+++ src/main/java/RestoApp/servicios/RestauranteServicio.java
@@ -1,85 +1,99 @@
-//
-//package RestoApp.servicios;
-//
-//import RestoApp.Entidades.Restaurante;
-//import RestoApp.Entidades.Zona;
-//import RestoApp.Errores.ErrorServicio;
-//import RestoApp.repositorios.RestauranteRepositorio;
-//import java.util.List;
-//import java.util.Optional;
-//import javax.transaction.Transactional;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class RestauranteServicio {
-//    
-//    @Autowired
-//    private RestauranteRepositorio restauranteRepositorio;
-//    
-//    @Transactional
-//    public void guardarRestaurante(String nombre, Menu menu,Integer mesas,Zona zona,Boolean abierto)throws ErrorServicio{
-//        
-//        Restaurante restaurante = new Restaurante();
-//        
-//        if (nombre == null || nombre.isEmpty()) {
-//            throw new ErrorServicio("El nombre no puede ser nulo");
-//        }
-//        if (menu == null || menu.isEmpty()) {
-//            throw new ErrorServicio("Se necesita un menu");
-//        }
-//        if (mesas == null || mesas == 0) {
-//            throw new ErrorServicio("Se necesita la cantidad de mesas");
-//        }
-//        if (zona == null) {
-//            throw new ErrorServicio("Se necesita saber la zona");
-//        }
-//        if (abierto == null ) {
-//            throw new ErrorServicio("Se necesita saber si esta abierto o cerrado");
-//        }
-//        
-//        
-//        restaurante.setNombre(nombre);
-//        restaurante.setMenu(menu);
-//        restaurante.setMesas(mesas);
-//        restaurante.setZona(zona);
-//        restaurante.setAbierto(abierto);
-//        
-//        restauranteRepositorio.save(restaurante);
-//        
-//    }
-//    
-//    @Transactional
-//    public Restaurante buscarRestaurante(String nombre){
-//        
-//       Restaurante restaurante = restauranteRepositorio.buscarRestaurantePorNombre(nombre);
-//       
-//       return restaurante;
-//        
-//    }
-//    
-//    @Transactional
-//    public void modificarRestaurante(String nombre) {
-//
-//        Restaurante restaurante = restauranteRepositorio.buscarRestaurantePorNombre(nombre);
-//        //restaurante.setNombre();
-//        restauranteRepositorio.save(restaurante);
-//
-//    }
-//    
-//    @Transactional
-//    public void eliminarRestaurante(String Id){
-//        
-//        Optional<Restaurante> restaurante = restauranteRepositorio.findById(Id);
-//        restaurante.get().setAbierto(false);
-//        restauranteRepositorio.save(restaurante.get());
-//        
-//    }
-//    
-//    @Transactional
-//    public List<Restaurante> listaraRestaurantes() {
-//        
-//        return restauranteRepositorio.findAll();
-//    
-//    }
-//}
+
+package RestoApp.servicios;
+
+import RestoApp.Entidades.Restaurante;
+import RestoApp.Entidades.Zona;
+import RestoApp.Errores.ErrorServicio;
+import RestoApp.repositorios.RestauranteRepositorio;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestauranteServicio {
+    
+    @Autowired
+    private RestauranteRepositorio restauranteRepositorio;
+    
+    @Transactional
+    public void guardarRestaurante(String nombre, Menu menu,Integer mesas,Zona zona,Boolean abierto)throws ErrorServicio{
+        
+        Restaurante restaurante = new Restaurante();
+        
+        if (nombre == null || nombre.isEmpty()) {
+            throw new ErrorServicio("El nombre no puede ser nulo");
+        }
+        if (menu == null || menu.isEmpty()) {
+            throw new ErrorServicio("Se necesita un menu");
+        }
+        if (mesas == null || mesas == 0) {
+            throw new ErrorServicio("Se necesita la cantidad de mesas");
+        }
+        if (zona == null) {
+            throw new ErrorServicio("Se necesita saber la zona");
+        }
+        if (abierto == null ) {
+            throw new ErrorServicio("Se necesita saber si esta abierto o cerrado");
+        }
+        
+        
+        restaurante.setNombre(nombre);
+        restaurante.setMenu(menu);
+        restaurante.setMesas(mesas);
+        restaurante.setZona(zona);
+        restaurante.setAbierto(abierto);
+        
+        restauranteRepositorio.save(restaurante);
+        
+    }
+    
+    @Transactional
+    public Restaurante buscarRestaurante(String nombre){
+        
+       Restaurante restaurante = restauranteRepositorio.buscarRestaurantePorNombre(nombre);
+       
+       return restaurante;
+        
+    }
+    
+    @Transactional
+    public Restaurante buscarRestauranteZona(String zona){
+        
+       Restaurante restaurante = restauranteRepositorio.buscarRestaurantePorZona(zona);
+       
+       return restaurante;
+        
+    }
+    
+    @Transactional
+    public void modificarRestaurante(String nombre,Menu menu,Integer mesas,Zona zona,Boolean abierto) {
+
+        Restaurante restaurante = restauranteRepositorio.buscarRestaurantePorNombre(nombre);
+        restaurante.setNombre(nombre);
+        restaurante.setAbierto(abierto);
+        restaurante.setMenu(menu);
+        restaurante.setMesas(mesas);
+        restaurante.setZona(zona);
+        restauranteRepositorio.save(restaurante);
+
+    }
+    
+    @Transactional
+    public void eliminarRestaurante(String Id){
+        
+        Optional<Restaurante> restaurante = restauranteRepositorio.findById(Id);
+        restaurante.get().setAbierto(false);
+        restauranteRepositorio.save(restaurante.get());
+        
+    }
+    
+    public List<Restaurante> listaraRestaurantes() {
+        
+        return restauranteRepositorio.findAll();
+    
+    }
+  
+    
+}
