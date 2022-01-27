a4e249997e2aab5a6eea26609ec01e774d5c085b
FedEgues <eguesf@gmail.com>
2022-01-26 22:03:29 -0300

    foto

--- src/main/java/RestoApp/Controladores/FotoControlador.java
+++ src/main/java/RestoApp/Controladores/FotoControlador.java
@@ -6,8 +6,11 @@
 package RestoApp.Controladores;
 
 import RestoApp.entidades.Plato;
+import RestoApp.servicios.ErrorServicio;
 import RestoApp.servicios.FotoServicio;
 import RestoApp.servicios.PlatoServicio;
+import com.sun.istack.logging.Logger;
+import java.util.logging.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -34,11 +37,15 @@ public class FotoControlador {
     @GetMapping("/fotoPlato")
      public ResponseEntity<byte[]> fotoPlato(String id){
         
+         try{
         Plato plato= pS.buscarPlatoId(id);
          byte[] foto =plato.getFoto().getContenido();
          HttpHeaders headers = new HttpHeaders();
          headers.setContentType(MediaType.IMAGE_JPEG);
          return new ResponseEntity<>(foto,headers,HttpStatus.OK);
+         }catch(ErrorServicio ex){
+             Logger.getLogger()
+         }
      }
              
      
